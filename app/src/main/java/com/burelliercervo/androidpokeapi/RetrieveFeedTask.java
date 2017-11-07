83d55e72a5b098cbfbd5fb27c7553af7c5286537
iem <iem@iutabgdin202-04.univ-lyon1.fr>
2017-11-07 16:07:38 +0100

    First commit

---
+++ app/src/main/java/com/burelliercervo/androidpokeapi/RetrieveFeedTask.java
@@ -0,0 +1,51 @@
+package com.burelliercervo.androidpokeapi;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by iem on 18/10/2017.
+ */
+
+class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+
+    protected String doInBackground(Void... urls) {
+        // Do some validation here
+
+        try {
+            URL url = new URL("https://pokeapi.co/api/v2/pokedex/1/");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        Log.i("INFO", response);
+        //responseView.setText(response);
+    }
+}
